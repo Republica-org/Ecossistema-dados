4e5f4f6d648f21cd5f863c601091ba9e53e92f3a
InstitutoRepublica <paula.frias@republica.org>
2023-06-30 22:37:12 -0300

    atualizando trabalho do dia 30/06

--- pre_tratamento/tratamento_republica/PNADC/geral.r
+++ pre_tratamento/tratamento_republica/PNADC/geral.r
@@ -3,6 +3,7 @@ library(PNADcIBGE)
 library(survey)
 
 
+
 setwd("G:\\Drives compartilhados\\República.org\\4. Equipes\\Dados e Comunicação\\DADOS\\460 - Infográficos\\07 - Mulheres no serviço público\\Pasta local\\pnadc_mulheres_servidoras")
 
 ## Variáveis escolhidas:
@@ -15,7 +16,6 @@ setwd("G:\\Drives compartilhados\\República.org\\4. Equipes\\Dados e Comunicaç
 #"V4028" = Nesse trabalho, ... era servidor público estatutário (federal, estadual ou municipal) ?
 # VD4016 = remuneracao
 #VD4010
-
 
 
 vars = c("V2007", "V2010", "V4028", "V4014", "V4012","V4010","VD4016", "VD4010")
@@ -73,3 +73,65 @@ sum(df$total)
 
 
 df[1,1]
+
+
+### Pegando os quartis para pirâmide ##Oficial
+
+teste_quantil4 <- svyquantile(x=~VD4016, design=subset(dadosPNADc_22,((V4012=="Empregado do setor público (inclusive empresas de economia mista)" ) & (V4028=="Sim") & (VD4016>1199))),quantiles=0.99842, ci=FALSE, na.rm=TRUE)
+
+teste_quantil4
+
+## Vendo os supersalários. 
+quantidade <- svytotal(x=~V4014, design=subset(dadosPNADc_22,((V4012=="Empregado do setor público (inclusive empresas de economia mista)" ) & (V4028=="Sim") & (VD4016>1199))), na.rm=TRUE)
+quantidade_super <- svytotal(x=~V4014, design=subset(dadosPNADc_22,((V4012=="Empregado do setor público (inclusive empresas de economia mista)" ) & (V4028=="Sim") & (VD4016>41650))), na.rm=TRUE)
+
+
+quantidade_super = data.frame(quantidade_super)
+quantidade = data.frame(quantidade)
+
+sum(quantidade_super$total)/sum(quantidade$total)*100
+
+
+#Até 70%: 
+#Até 5.000
+
+#Até 90%:
+#até 10.000
+
+#Até 95%
+#até 15000
+
+# Até 99%
+#27000
+
+#Até 99,85%
+#39200
+
+
+
+### Verificar supersalarios
+### Vendo os dados da pirâmide!! 
+#quantidade total: 
+6927827+1628540
+
+quantidade = svytotal(x=~V4028, design=subset(dadosPNADc_22,V4012=="Empregado do setor público (inclusive empresas de economia mista)" ), na.rm=TRUE)
+
+
+quantidade_estatutarios = svytotal(x=~V4012, design=subset(dadosPNADc_22,V4028=="Sim"), na.rm=TRUE)
+quantidade_estatutarios_teto = svytotal(x=~V4012, design=subset(dadosPNADc_22,VD4016>39200 & V4028=="Sim"), na.rm=TRUE)
+
+df = data.frame(quantidade)
+view(df_estatutario)
+
+
+df_total = data.frame(quantidade_com_militares)
+df_estatutario = data.frame(quantidade_estatutarios)
+df_quantidade_estatutarios_teto = data.frame(quantidade_estatutarios_teto)
+
+view(df_estatutario)
+view(quantidade_estatutarios_teto)
+
+## 2022: 
+#10708/6927827
+#0,15%
+
