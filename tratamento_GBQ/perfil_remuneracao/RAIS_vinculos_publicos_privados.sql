e4c4534c951d85feb75bab178de82a130f923f58
alobogiron <andregiron.20221@poli.ufrj.br>
2025-09-04 18:58:55 -0300

    RAIS vinculos publico privados

--- tratamento_GBQ/perfil_remuneracao/RAIS_vinculos_publicos_privados.sql
+++ tratamento_GBQ/perfil_remuneracao/RAIS_vinculos_publicos_privados.sql
@@ -1,47 +1,45 @@
-SELECT ano, sexo, poderes, 
-
- CASE 
-    WHEN grau_instrucao in ('Analfabeto','Até 5.a inc', '5.a completo ','6.a ao 9.a fund','Fund completo','Médio incompleto') THEN 'Até Fundamental '
-    WHEN grau_instrucao in ('Médio completo','Sup. incompleto') THEN 'Até Ensino Médio'
-    WHEN grau_instrucao in ('Sup. completo') THEN 'Até Ensino Superior Completo'
-    WHEN grau_instrucao in ('Mestrado') THEN 'Até Mestrado'
-    WHEN grau_instrucao in ('Doutorado') THEN 'Até Doutorado'
-    ELSE 'Ignorado' 
-
+-- Seleciona e agrupa os dados de vínculos públicos
+SELECT
+  ano,
+  sexo,
+  poderes,
+  CASE
+    WHEN grau_instrucao IN ('Analfabeto', 'Até 5.a inc', '5.a completo ', '6.a ao 9.a fund', 'Fund completo', 'Médio incompleto') THEN 'Até Fundamental'
+    WHEN grau_instrucao IN ('Médio completo', 'Sup. incompleto') THEN 'Até Ensino Médio'
+    WHEN grau_instrucao IN ('Sup. completo') THEN 'Até Ensino Superior Completo'
+    WHEN grau_instrucao IN ('Mestrado') THEN 'Até Mestrado'
+    WHEN grau_instrucao IN ('Doutorado') THEN 'Até Doutorado'
+    ELSE 'Ignorado'
   END AS grau_instrucao,
+  esfera,
+  sigla_uf,
+  "Público" AS flag_publico_privado,
+  SUM(quantidade_vinculos) AS quantidade_vinculos
+FROM
+  `repositoriodedadosgpsp.Datalake.Republica_vinculos_publicos`
+GROUP BY
+  1, 2, 3, 4, 5, 6, 7  -- Agrupando pela posição das colunas
 
-esfera, 
+UNION ALL
 
-sigla_uf,  "Público" as flag_publico_privado, sum(quantidade_vinculos) as quantidade_vinculos
-
-FROM `repositoriodedadosgpsp.Datalake.Republica_vinculos_publicos` 
-
-group by  ano, sexo,poderes, grau_instrucao,esfera,flag_publico_privado, sigla_uf
-
-
-union all
-
-SELECT ano, sexo, poderes, 
-
- CASE 
-    WHEN grau_instrucao in ('Analfabeto','Até 5.a inc', '5.a completo ','6.a ao 9.a fund','Fund completo','Médio incompleto') THEN 'Até Fundamental '
-    WHEN grau_instrucao in ('Médio completo','Sup. incompleto') THEN 'Até Ensino Médio'
-    WHEN grau_instrucao in ('Sup. completo') THEN 'Até Ensino Superior Completo'
-    WHEN grau_instrucao in ('Mestrado') THEN 'Até Mestrado'
-    WHEN grau_instrucao in ('Doutorado') THEN 'Até Doutorado'
-    ELSE 'Ignorado' 
-
+-- Seleciona e agrupa os dados de vínculos privados
+SELECT
+  ano,
+  sexo,
+  poderes,
+  CASE
+    WHEN grau_instrucao IN ('Analfabeto', 'Até 5.a inc', '5.a completo ', '6.a ao 9.a fund', 'Fund completo', 'Médio incompleto') THEN 'Até Fundamental'
+    WHEN grau_instrucao IN ('Médio completo', 'Sup. incompleto') THEN 'Até Ensino Médio'
+    WHEN grau_instrucao IN ('Sup. completo') THEN 'Até Ensino Superior Completo'
+    WHEN grau_instrucao IN ('Mestrado') THEN 'Até Mestrado'
+    WHEN grau_instrucao IN ('Doutorado') THEN 'Até Doutorado'
+    ELSE 'Ignorado'
   END AS grau_instrucao,
-
-esfera, 
-
-sigla_uf,"Privado" as flag_publico_privado, sum(quantidade_vinculos) as quantidade_vinculos
-
-FROM `repositoriodedadosgpsp.Datalake.RAIS_vinculos_privados` 
-
-group by  ano, sexo,poderes, grau_instrucao,esfera,flag_publico_privado, sigla_uf
-
-
-
-
-
+  esfera,
+  sigla_uf,
+  "Privado" AS flag_publico_privado,
+  SUM(quantidade_vinculos) AS quantidade_vinculos
+FROM
+  `repositoriodedadosgpsp.Datalake.RAIS_vinculos_privados`
+GROUP BY
+  1, 2, 3, 4, 5, 6, 7; -- Agrupando pela posição das colunas
